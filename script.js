77055b77e22dfdf4c86091018468339ea81017c7
kauemello <kauemello@users.noreply.github.com>
2022-03-16 22:13:24 -0300

    projeto_envio
    projeto_final_envio

---
+++ script.js
@@ -0,0 +1,16 @@
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  draggable: true,
+  
+  rewind: true,
+
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+ 
+ 
